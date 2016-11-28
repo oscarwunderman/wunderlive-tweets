128979e7bd4b2285e05687ca37670475353b20b3
Oscar Fernández <oscar.fernandez@wunderman.com>
2016-11-28 17:53:56 +0100

    set_time_limit(0);

--- server/config/conf.php
+++ server/config/conf.php
@@ -35,3 +35,4 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 header('Access-Control-Allow-Origin: http://wundermanlive.herokuapp.com');
+set_time_limit(0);
