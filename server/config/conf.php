269fc2249db4f56e6d43db30f9fb9a9a57947eb6
Oscar Fernández <oscar.fernandez@wunderman.com>
2016-11-28 13:50:52 +0100

    cambiada urlde cabecera a: http://wundermanlive.herokuapp.com

--- server/config/conf.php
+++ server/config/conf.php
@@ -34,4 +34,4 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
-header('Access-Control-Allow-Origin: http://wunderlive.herokuapp.com');
+header('Access-Control-Allow-Origin: http://wundermanlive.herokuapp.com');
